8097f3bb64afd84d1cb8f83e12b14cbde12be3fe
Roman Karandashov <r.karandashov@haulmont.com>
2021-02-20 16:56:03 +0400

    Modified webpack config in react-scripts

---
+++ packages/react-scripts/config/webpack.config-haulmont.js
@@ -0,0 +1,40 @@
+const { join } = require('path');
+const webpackConfig = require('./webpack.config');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+const isDevMode = process.env.IS_DEVMODE === 'true';
+
+module.exports = function (webpackEnv) {
+  const craConfig = webpackConfig(webpackEnv);
+  const { plugins } = craConfig;
+  const [htmlWebpackPlugin] = plugins;
+
+  htmlWebpackPlugin.options.isDevMode = isDevMode;
+
+  if (isDevMode) {
+    const pathToReactDevtools = join(
+      '@haulmont',
+      'react-ide-devtools',
+      'dist',
+      'devtools-no-server.js'
+    );
+    const pathToNodeModules = join(__dirname, '..', '..');
+    const pathToReactDevtoolsScript = join(
+      pathToNodeModules,
+      pathToReactDevtools
+    );
+
+    plugins.push(
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: pathToReactDevtoolsScript,
+            to: 'static/js',
+          },
+        ],
+      })
+    );
+  }
+
+  return craConfig;
+};
