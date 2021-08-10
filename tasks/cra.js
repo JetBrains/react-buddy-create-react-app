79838656c0d3d7e42d33c9698f06ebb9ec5982cd
Roman Karandashov <r.karandashov@haulmont.com>
2021-08-10 16:20:56 +0400

    fixed issues with yarn cache clean

--- tasks/cra.js
+++ tasks/cra.js
@@ -95,7 +95,7 @@ const scriptsFileName = cp
 const scriptsPath = path.join(packagesDir, 'react-scripts', scriptsFileName);
 
 // Now that we have packed them, call the global CLI.
-cp.execSync('yarn cache clean');
+cp.execSync('npx yarn cache clean');
 
 const args = process.argv.slice(2);
 
