79c2087990a23ce39d4ac00574aa47d5a4015d92
Roman Karandashov <r.karandashov@haulmont.com>
2022-01-20 14:05:27 +0400

    feat: add palette component to preview

--- packages/cra-template-typescript/template/src/dev/previews.tsx
+++ packages/cra-template-typescript/template/src/dev/previews.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Previews, ComponentPreview } from '@react-buddy/ide-toolbox';
+import Palette from './palette';
 import App from '../App';
 
 export const ComponentPreviews = () => {
   return (
-    <Previews>
+    <Previews palette={<Palette />}>
       <ComponentPreview path="/App">
         <App />
       </ComponentPreview>
